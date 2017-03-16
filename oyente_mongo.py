57ef1cb15e3e44a97a740d04443d67a14ac51e6e
nud3l <dominik.harz@gmail.com>
2017-03-16 12:22:31 +0100

    Include MongoDB loader

--- oyente_mongo.py
+++ oyente_mongo.py
@@ -5,10 +5,18 @@ import re
 import sys
 import global_params
 import argparse
+from helpers.config import config
+from helpers.models import Code
+import pymongo
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import json
+
 
 def cmd_exists(cmd):
     return subprocess.call("type " + cmd, shell=True,
         stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
+
 
 def has_dependencies_installed():
     try:
@@ -27,6 +35,7 @@ def has_dependencies_installed():
         return False
 
     return True
+
 
 def main():
     # Runs through batch of documents stored in MongoDB
@@ -80,45 +89,96 @@ def main():
     #
     #     return
 
-    # get all contracts
-    from helpers.config import config
-    
+    # connect MongoDB
+    mongo_db = pymongo.MongoClient()
+    code_base = mongo_db.code.code_base
+
+    # get all code ids
+    code_ids = code_base.find({}, {})
+    total_code_number = code_ids.count()
+
+    # create SQL connection
+    sql = config['sql']
+    engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(
+            sql['user'],
+            sql['password'],
+            sql['host'],
+            sql['port'],
+            sql['db']
+    ))
+    Session = sessionmaker(bind=engine)
+    sql_db = Session()
 
 
     # Compile first
     solc_cmd = "solc --optimize --bin-runtime %s"
 
-    FNULL = open(os.devnull, 'w')
+    i = 0
+    for code in code_ids:
+        i += 1
+        print("Analysing contract id {} -- {} of {}".format(code["_id"], i, total_code_number))
 
-    solc_p = subprocess.Popen(shlex.split(solc_cmd % args.source), stdout = subprocess.PIPE, stderr=FNULL)
-    solc_out = solc_p.communicate()
+        code_item = sql_db.query(Code).filter(Code.id == code["_id"]).first()
+        code_file = code_item.code
 
-    for (cname, bin_str) in re.findall(r"\n======= (.*?) =======\nBinary of the runtime part: \n(.*?)\n", solc_out[0]):
-        print "Contract %s:" % cname
-        bin_str += "\0"
+        FNULL = open(os.devnull, 'w')
 
-        disasm_out = ""
+        print solc_cmd % code_file
+
+        solc_p = subprocess.Popen(shlex.split(solc_cmd % code_file), stdout = subprocess.PIPE, stderr=FNULL)
+        solc_out = solc_p.communicate()
+
+        for (cname, bin_str) in re.findall(r"\n======= (.*?) =======\nBinary of the runtime part: \n(.*?)\n", solc_out[0]):
+            print "Contract %s:" % cname
+            bin_str += "\0"
+
+            disasm_out = ""
+            try:
+                disasm_p = subprocess.Popen(shlex.split('disasm'), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+                disasm_out = disasm_p.communicate(input=bin_str)[0]
+
+            except:
+                print "Disassembly failed."
+
+            # Run symExec
+
+            with open(cname+'.evm.disasm', 'w') as of:
+                of.write(disasm_out)
+
+            # TODO: Do this as an import and run, instead of shell call and hacky fix
+
+            os.system('python symExec.py %s.evm.disasm %d %d %d %d %d %d %d %d %d %d %s' % (
+                cname,
+                global_params.IGNORE_EXCEPTIONS,
+                global_params.REPORT_MODE,
+                global_params.PRINT_MODE,
+                global_params.DATA_FLOW,
+                global_params.DEBUG_MODE,
+                global_params.CHECK_CONCURRENCY_FP,
+                global_params.TIMEOUT,
+                global_params.UNIT_TEST,
+                global_params.GLOBAL_TIMEOUT,
+                global_params.PRINT_PATHS,
+                cname+".json"))
+
+            # if args.evm:
+            #     with open(cname+'.evm','w') as of:
+            #         of.write(bin_str)
+
+            os.system('rm %s.evm.disasm' % (cname))
+
         try:
-            disasm_p = subprocess.Popen(shlex.split('disasm'), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-            disasm_out = disasm_p.communicate(input=bin_str)[0]
+            json_code = dict()
+            # Update MongoDB
+            with open("{}.json".format(cname)) as json_file:
+                json_code = json.load(json_file)
+            print json_code
 
-        except:
-            print "Disassembly failed."
+            code_base.update({'_id': code['_id']},
+                             {'$set': {"security": json_code}})
 
-        # Run symExec
-
-        with open(cname+'.evm.disasm', 'w') as of:
-            of.write(disasm_out)
-
-        # TODO: Do this as an import and run, instead of shell call and hacky fix
-
-        os.system('python symExec.py %s.evm.disasm %d %d %d %d %d %d %d %d %d %d %s' % (cname, global_params.IGNORE_EXCEPTIONS, global_params.REPORT_MODE, global_params.PRINT_MODE, global_params.DATA_FLOW, global_params.DEBUG_MODE, global_params.CHECK_CONCURRENCY_FP, global_params.TIMEOUT, global_params.UNIT_TEST, global_params.GLOBAL_TIMEOUT, global_params.PRINT_PATHS, cname+".json" if args.json else ""))
-
-        if args.evm:
-            with open(cname+'.evm','w') as of:
-                of.write(bin_str)
-
-        os.system('rm %s.evm.disasm' % (cname))
+        except UnboundLocalError as e:
+            print "{}: code id {} security analysis failed".format(e, code["_id"])
 
 
 if __name__ == '__main__':
